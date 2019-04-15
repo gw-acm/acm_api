192282b0b7e5acf0281492e6a26da454e33d9fd0
AaronCoplan <afcoplan@gmail.com>
2019-04-14 23:04:54 -0400

    Adding API code to interact with Google Drive.

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route('/acm-events-images')
+def gdrive():
+    apiKey = config['google-drive']['api-key']
+    folderId = config['google-drive']['folder-id']
+    driveURL = "https://www.googleapis.com/drive/v3/files?q='{}'+in+parents&key={}".format(apiKey, folderId)
+    return requests.get(driveURL).text
+
+def loadconfig():
+    with open('./config.json', 'r+') as configFile:
+        config = configFile.read()
+        return json.loads(config)
+
+if __name__ == '__main__':
+    config = loadconfig()
+    app.run()
